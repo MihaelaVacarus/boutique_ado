f0c0721dc8a54374b15d2b6891b1107755b1e07c
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-08-01 11:12:20 +0000

    Product Admin - Editing Products

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,9 +4,9 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
-    
+
     bag_items = []
-    total = 0 
+    total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
@@ -40,7 +40,7 @@ def bag_contents(request):
         free_delivery_delta = 0
     
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
